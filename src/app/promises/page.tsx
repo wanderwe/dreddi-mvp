0d3895ce6f1ad4df1d276e592d875371607fd062
Ihor Allaverdiiev <allaverdiiev@gmail.com>
2026-01-04 20:30:03 +0200

    fix(build): force dynamic for /promises

--- src/app/promises/page.tsx
+++ src/app/promises/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import Link from "next/link";
 import { useEffect, useMemo, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
